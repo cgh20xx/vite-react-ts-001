9f76a1151478ae56f803553c7a3d8b5498cb174e
Hank <cgh20xx@gmail.com>
2023-06-06 19:05:25 +0800

    feat: 新增 paths alias 在 tsconfig 讓 vscode 能看的懂 @

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // 讓 index.html 裡的 src="/assets/xxx"改為相對路徑 src="./assets/xxx"
   plugins: [react()],
   resolve: {
     alias: {
