bb2df32fa9b0c50a21911fd7373aeef0a8d333fe
Hank <cgh20xx@gmail.com>
2023-06-08 21:45:57 +0800

    feat: 使用 useState 來改變狀態自動更新畫面

--- src/ch03/ex4/App.tsx
+++ src/ch03/ex4/App.tsx
@@ -1,15 +1,20 @@
+import { useState } from "react"
+
 // 定義 props 物件的型別
 type BtnProps = {
   total: number
 }
 
 // 在 ts 中 props 必需傳入一個泛型告訴裡面的物件是什麼型別。
-const Btn: React.FC<BtnProps> = ({total}) => {
+// 讓組件有狀態(state)，改變狀態 -> 自動觸發畫面更新
+const Btn: React.FC<BtnProps> = (props) => {
+  const [total, setTotal] = useState(props.total)
   function add() {
-    total++
-    console.log(total);
-    // 注意：觀察 console.log total 有被更新，但畫面沒有重新 render。
+    // 使用 setTotal 更新值則會更新畫面
+    setTotal(total + 1)
   }
+  console.log(total);
+  // 注意：畫面會更新 2 次是因為 React.StrictMode 在 dev 環境的關係，不要緊。
   return (
     // 注意：原生 js 使用 onclick，react 使用 onClick。 
     <button onClick={add}>
