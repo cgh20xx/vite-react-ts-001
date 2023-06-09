63f27cf8cd3adee848ca21eb4fa2ced73712f065
Hank <cgh20xx@gmail.com>
2023-06-09 16:06:04 +0800

    feat: 新增 ch03/ex5 Class Component 起手範例

---
+++ src/ch03/ex5/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// class component 起手式
+type AppProps = object
+type AppState = object
+
+class App extends React.Component<AppProps, AppState> {
+  constructor(props: AppProps) {
+    super(props)
+  }
+}
+
+export default App
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import App from '@/ch03/ex1/App'
 // import App from '@/ch03/ex1/App2'
-import App from '@/ch03/ex4/App'
+// import App from '@/ch03/ex4/App'
+import App from '@/ch03/ex5/App'
+
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
