22122f8a394a18d96620aa4b8e7b15405cc124fb
Hank <cgh20xx@gmail.com>
2023-06-09 17:59:58 +0800

    feat: 新增使用 setState 來更新狀態

--- src/ch03/ex5/App.tsx
+++ src/ch03/ex5/App.tsx
@@ -20,6 +20,7 @@ class Btn extends React.Component<BtnProps, BtnState> {
 // App 元件
 type AppProps = object
 type AppState = {
+  title: string
   count: number
 }
 
@@ -29,8 +30,13 @@ class App extends React.Component<AppProps, AppState> {
     console.log('constructor');
     // 新增一個名為 count 的 state
     this.state = {
+      title: '計數器',
       count: 0
     }
+
+    // 下面這行是保証 method 裡的 this 永遠指向 App
+    // 但缺點是 app1.countClickHandler 和 app2.countClickHandler 就指向不同的記憶體位址。
+    this.countClickHandler = this.countClickHandler.bind(this)
   }
   // 以下有三個最常用的生命週期方法
 
@@ -41,7 +47,7 @@ class App extends React.Component<AppProps, AppState> {
 
   // 2. Component 更新完之後
   componentDidUpdate(prevProps: object, prevState: object, snapshot?: any): void {
-    console.log('componentDidUpdate');
+    console.log('componentDidUpdate', this.state);
   }
 
   // 3. Component 將被刪除之前
@@ -52,18 +58,40 @@ class App extends React.Component<AppProps, AppState> {
 
   // 自定義一個 countClickHandler() method
   countClickHandler() {
-    console.log('countClickHandler');
+    console.log('countClickHandler', this);
+    // 注意：countClickHandler 由於是在 Btn onClick 才被呼叫，
+    // 這裡拿到的 this 在未經處理(bind)的情況，不是 App 的 instance。
+    // 需要將 countClickHandler 強制綁定 App 的 this 才符合理想中的 this。
+
+    // 要更新 State 需使用 setState，並回傳一個新的 State。
+
+    // 方法1：
+    // this.setState({
+    //   count: this.state.count + 1
+    // })
+
+    // 方法2：
+    this.setState((prevState) => {
+      return {
+        count: prevState.count + 1
+      }
+    })
   }
 
   // overrid React.Component 的 render 方法
   render(): React.ReactNode {
     return (
       <>
-        <h1>Count: {this.state.count}</h1>
+        <h1>{this.state.title} Count: {this.state.count}</h1>
         <Btn clickHandler={this.countClickHandler} />
       </>
     )
   }
 }
+
+const app1 = new App({})
+console.log('app1:', app1);
+const app2 = new App({})
+console.log('app2:', app2);
 
 export default App
