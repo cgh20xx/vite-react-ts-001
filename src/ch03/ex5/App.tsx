84e1953886f370cd59ce112841aae369eb1bfd97
Hank <cgh20xx@gmail.com>
2023-06-09 16:11:49 +0800

    feat: 新增 App 及 Btn 元件的 html 模板(tsx)

--- src/ch03/ex5/App.tsx
+++ src/ch03/ex5/App.tsx
@@ -1,12 +1,37 @@
 import React from 'react'
 
-// class component 起手式
+// Btn 元件
+type BtnProps = object
+type BtnState = object
+
+class Btn extends React.Component<BtnProps, BtnState> {
+  constructor(props: BtnProps) {
+    super(props)
+  }
+
+  // overrid React.Component 的 render 方法
+  render(): React.ReactNode {
+    return <button>+1</button>
+  }
+}
+
+// App 元件
 type AppProps = object
 type AppState = object
 
 class App extends React.Component<AppProps, AppState> {
   constructor(props: AppProps) {
     super(props)
+  }
+
+  // overrid React.Component 的 render 方法
+  render(): React.ReactNode {
+    return (
+      <>
+        <h1>Count: 0</h1>
+        <Btn />
+      </>
+    )
   }
 }
 
