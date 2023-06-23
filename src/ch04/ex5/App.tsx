e18ec908cabffe8cae8c03917a6efdd753fa8c12
Hank <cgh20xx@gmail.com>
2023-06-23 08:53:17 +0800

    feat: 新增 cleanup function 在 useEffect 的 setup function 中，以清除事件，避免每次更新 state 時重覆綁定事件。

--- src/ch04/ex5/App.tsx
+++ src/ch04/ex5/App.tsx
@@ -1,3 +1,4 @@
+// 此範例功能：每次按下 +1 就重製計時器 num = 0
 import { useState, useEffect } from 'react'
 
 let interval: NodeJS.Timeout
@@ -11,10 +12,19 @@ const App: React.FC = () => {
     console.log('useEffect: [counter]');
     // 如果 useEffect 的 dependencies 有 state 時，偵聽事件可能會被多次綁定。
     // 如下：setInterval 會在 counter 每次被更新時重覆綁定。
+    // 所以需要 return cleanup function 並在裡面清除事件
     interval = setInterval(() => {
       num++
       console.log(num)
     }, 1000)
+    // cleanup function：在 component 被從 DOM 移除時，會執行 cleanup function。
+    return () => {
+      console.log('before re-render');
+      if (interval) {
+        clearInterval(interval)
+        num = 0
+      }
+    }
   }, [counter])
 
   function clickHandler() {
