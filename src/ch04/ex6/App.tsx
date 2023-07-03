9a8410ef5b1f70061f3b1264cd036a34349c394b
Hank <cgh20xx@gmail.com>
2023-07-03 20:25:46 +0800

    feat: 修改 postId 改為傳入參數而不寫死，新增按鈕可指定 postId

--- src/ch04/ex6/App.tsx
+++ src/ch04/ex6/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 // 為 API 回傳的 data 內容定義一個 interface or type
 interface Comments {
@@ -11,27 +11,30 @@ interface Comments {
 
 const App: React.FC = () => {
 
+  const [postId, setPostId] = useState(1)
+  console.log('postId:', postId);
+
+  // 使用 async await 改寫 fetch
+  // 注意： useEffect 第一個參數不支持 async function，故必需定義另一個 async function 並呼叫它。
+  async function fetchData(id: number) {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/comments?postId=${id}`)
+    const data = await res.json() as Comments[]
+    console.log('data2:', data);
+  }
+
   // 使用 fetch 打一個假的 api
   useEffect(() => {
-    // fetch('https://jsonplaceholder.typicode.com/comments?postId=1')
-    // .then(res => res.json())
-    // .then((data: Comments[]) => {
-    //   console.log('data:', data);
-    // })
+    fetchData(postId)
+  }, [postId])
 
-    // 使用 async await 改寫 fetch
-    // 注意： useEffect 第一個參數不支持 async function，故必需定義另一個 async function 並呼叫它。
-    async function fetchData() {
-      const res = await fetch('https://jsonplaceholder.typicode.com/comments?postId=1')
-      const data = await res.json() as Comments[]
-      console.log('data2:', data);
-    }
-
-    fetchData()
-  }, [])
+  function clickHandler(id: number) {
+    setPostId(id)
+  }
 
   return <>
     <h1>Fetch</h1>
+    <button onClick={() => clickHandler(1)}>ID 1 data</button>
+    <button onClick={() => clickHandler(2)}>ID 2 data</button>
   </>
 }
 
