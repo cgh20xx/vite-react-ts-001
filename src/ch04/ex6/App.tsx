803d998b376a280b06af842ca82eb9ff2d4c8b4c
Hank <cgh20xx@gmail.com>
2023-07-03 20:03:38 +0800

    feat: 新增一個 interface 給打 api 後回傳的資料。

--- src/ch04/ex6/App.tsx
+++ src/ch04/ex6/App.tsx
@@ -1,5 +1,13 @@
 import { useEffect } from 'react'
 
+// 為 API 回傳的 data 內容定義一個 interface or type
+interface Comments {
+  postId: number
+  id: number
+  name: string
+  email: string
+  body: string
+}
 
 const App: React.FC = () => {
   
@@ -7,7 +15,7 @@ const App: React.FC = () => {
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/comments?postId=1')
     .then(res => res.json())
-    .then(data => {
+    .then((data: Comments[]) => {
       console.log('data:', data);
     })
   }, [])
