2d85b583869afc05acfc0b758eef57eea8ee3041
Hank <cgh20xx@gmail.com>
2023-07-03 20:10:05 +0800

    feat: 改寫 fetch promise 寫法，使用 async await。

--- src/ch04/ex6/App.tsx
+++ src/ch04/ex6/App.tsx
@@ -10,14 +10,24 @@ interface Comments {
 }
 
 const App: React.FC = () => {
-  
+
   // 使用 fetch 打一個假的 api
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/comments?postId=1')
-    .then(res => res.json())
-    .then((data: Comments[]) => {
-      console.log('data:', data);
-    })
+    // fetch('https://jsonplaceholder.typicode.com/comments?postId=1')
+    // .then(res => res.json())
+    // .then((data: Comments[]) => {
+    //   console.log('data:', data);
+    // })
+
+    // 使用 async await 改寫 fetch
+    // 注意： useEffect 第一個參數不支持 async function，故必需定義另一個 async function 並呼叫它。
+    async function fetchData() {
+      const res = await fetch('https://jsonplaceholder.typicode.com/comments?postId=1')
+      const data = await res.json() as Comments[]
+      console.log('data2:', data);
+    }
+
+    fetchData()
   }, [])
 
   return <>
