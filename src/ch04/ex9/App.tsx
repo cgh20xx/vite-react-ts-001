ab2b2cd6e561ace21b62da84eba4ba562f7983f0
Hank <cgh20xx@gmail.com>
2023-07-05 22:48:38 +0800

    feat: 新增 useRef 配合 ref 取得 Dom 是官方建議的方式。

--- src/ch04/ex9/App.tsx
+++ src/ch04/ex9/App.tsx
@@ -5,16 +5,19 @@ import { useEffect, useRef } from 'react'
 
 const App: React.FC = () => {
 
+  const textRef = useRef<HTMLInputElement>(null) // 預設值必需為 null
+
   // 使用 useEffect 才可正確拿到組件的 Dom
   useEffect(() => {
-    const textDom = document.getElementById('text') as HTMLInputElement;
-    console.log(textDom); // 可拿到 input 的 dom 了
-    textDom.focus()
+    // const textDom = document.getElementById('text') as HTMLInputElement;
+    console.log(textRef.current); // 可拿到 input 的 dom 
+    textRef.current?.focus()
   }, [])
 
   return <>
     <h1>Ref DOM</h1>
-    <input id="text" type="text" />
+    {/* <input id="text" type="text" /> */}
+    <input ref={textRef} type="text" />
   </>
 }
 
