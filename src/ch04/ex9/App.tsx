57851d78a2d2bc5f4a1955fb988cde47ecb3d7c3
Hank <cgh20xx@gmail.com>
2023-07-05 22:33:26 +0800

    feat: 新增 useEffect 才可正確拿到 Dom，使用 getElementById 方式。

--- src/ch04/ex9/App.tsx
+++ src/ch04/ex9/App.tsx
@@ -1,12 +1,16 @@
-import { useRef } from 'react'
+import { useEffect, useRef } from 'react'
 
 // 情境：取得 HTML DOM 元素的方式
 // 第一種使用傳統的 document.getElementById('id');
 
 const App: React.FC = () => {
 
-  const textDom = document.getElementById('text');
-  console.log(textDom); // null
+  // 使用 useEffect 才可正確拿到組件的 Dom
+  useEffect(() => {
+    const textDom = document.getElementById('text') as HTMLInputElement;
+    console.log(textDom); // 可拿到 input 的 dom 了
+    textDom.focus()
+  }, [])
 
   return <>
     <h1>Ref DOM</h1>
