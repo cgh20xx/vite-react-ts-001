627d83360a8e3eb5a85eb84d44cabe529651c5a8
Hank <cgh20xx@gmail.com>
2023-07-05 22:00:23 +0800

    feat: 新增使用 useRef 來定義 sum 變數，可解決 rerender 時，變數被重置的問題。

--- src/ch04/ex8/App.tsx
+++ src/ch04/ex8/App.tsx
@@ -2,20 +2,24 @@ import { useState, useRef } from 'react'
 
 // 情境：寫一個計數(不需更新畫面)，點5次顯示隱藏的區塊，點15次關閉區塊。
 // 問題：呼叫顯示隱藏的 setHidden 會觸發 rerender 導致變數被重罝。
+// 改善：使用 useRef() 來改寫 sum 可解決以上問題。
+// useRef 與 useState 的不同在於 useRef 更新值時不會 rerender
 const App: React.FC = () => {
 
   const [hidden, setHidden] = useState(false)
-
-  let sum = 0
-  console.log('init sum:', sum);
+  // 使用 useRef 取代 sum 讓 react 在 rerender 能記住之前的值
+  const sumRef = useRef(0)
+  // 注意： useRef() 回傳的是物件，需使用 .current 取得值
+  // let sum = 0
+  console.log('init sumRef.current:', sumRef.current);
   console.log('init hidden:', hidden);
 
   function clickHandler() {
-    sum = sum + 1
-    console.log('click sum:', sum);
-    if (sum === 5) {
+    sumRef.current = sumRef.current + 1
+    console.log('click sumRef.current:', sumRef.current);
+    if (sumRef.current === 5) {
       setHidden(true)
-    } else if (sum > 15) {
+    } else if (sumRef.current > 15) {
       // 這邊會有問題，因 sum 會被重置了 1 次才會跑到 6 以上。
       setHidden(false)
     }
