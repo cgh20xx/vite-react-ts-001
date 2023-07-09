878f9fec29ff6e18c20a8d0c32f5840311937cfb
Hank <cgh20xx@gmail.com>
2023-07-09 14:45:59 +0800

    feat: 新增表單改變 input value 雙相綁定 state

--- src/ch04/ex10/App.tsx
+++ src/ch04/ex10/App.tsx
@@ -1,9 +1,32 @@
+import { useState } from 'react';
+
 const App: React.FC = () => {
+
+  const [account, setAccount] = useState('')
+  const [password, setPassword] = useState('')
+
+  function accountHandler(event: React.ChangeEvent<HTMLInputElement>) {
+    // e.target：指的是觸發事件的物件。
+    // e.currentTarget：指的是被監聽的物件。(通常與 this 相同)
+    setAccount(event.currentTarget.value)
+  }
+
+  function passwordHandler(event: React.ChangeEvent<HTMLInputElement>) {
+    setPassword(event.currentTarget.value)
+  }
+
+  function sendFromHandler() {
+    console.log('account:', account);
+    console.log('password:', password);
+  }
+
   return <>
     <h1>登入</h1>
-    <p>帳號：<input type="text" name="account" /></p>
-    <p>密碼：<input type="password" name="password" /></p>
-    <input type="button" value="送出" />
+    <p>帳號：<input type="text" value={account} name="account" onChange={accountHandler} /></p>
+    <p>{account}</p>
+    <p>密碼：<input type="password" value={password} name="password" onChange={passwordHandler} /></p>
+    <p>{password}</p>
+    <input type="button" value="送出" onClick={sendFromHandler} />
   </>
 }
 
