9643496df7e5866cb2039f899561042690f99820
Hank <cgh20xx@gmail.com>
2023-07-11 17:33:49 +0800

    feat: 新增使用 useMemo 記憶 obj 改善原本每次 rerender 都會是新的 obj 物件。

--- src/ch05/ex3/App.tsx
+++ src/ch05/ex3/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useMemo } from 'react'
 
 const App: React.FC = () => {
   console.log('App render');
@@ -7,12 +7,21 @@ const App: React.FC = () => {
   // 假設自訂一個 obj 放到 deps array 中，eslint 會提示 useEffect 會在每次 render 時被執行
   // 因為次 render 時 obj 都是一個新的物件，所以每次都會被更新。
   // 建議 obj 使用 useMemo() 重新包裝。
-  const obj = {}
+  // 使用 useMemo 重新包裝 obj 後，就會記住 obj，而不會每次 rerender 都是不同 obj，造成一直觸發 useEffect callback
 
-  // 當 deps array 觀注 value 參數，每次 value 改變就會執行 useEffect callback
+  // useMemo will only recompute the memoized value when one of the deps has changed.
+  // useMemo 文件：https://react.dev/reference/react/useMemo
+  const memoObj = useMemo(() => {
+    const obj = { name: 'Hank' }
+    return obj
+  }, [])
+
+  // console.log('memoObj:', memoObj); // { name: 'Hank' }
+
+  // 修改 deps array 關注 memoObj 參數後，useEffect callback 就只會執行一次了。
   useEffect(() => {
     console.log('useEffect callback');
-  }, [obj])
+  }, [memoObj])
 
   return <>
     <h1>App</h1>
