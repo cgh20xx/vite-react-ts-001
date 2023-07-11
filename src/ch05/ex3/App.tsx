a881329f677d98aed9d383bbbf892a23918bbcf8
Hank <cgh20xx@gmail.com>
2023-07-11 17:16:32 +0800

    feat: 新增 obj 物件到 useEffect 的 deps array 中會導致 useEffect callback 每次都會被執行，下個範列將使用 useMemo 包裝 obj。

--- src/ch05/ex3/App.tsx
+++ src/ch05/ex3/App.tsx
@@ -4,10 +4,15 @@ const App: React.FC = () => {
   console.log('App render');
   const [value, setValue] = useState(false)
 
+  // 假設自訂一個 obj 放到 deps array 中，eslint 會提示 useEffect 會在每次 render 時被執行
+  // 因為次 render 時 obj 都是一個新的物件，所以每次都會被更新。
+  // 建議 obj 使用 useMemo() 重新包裝。
+  const obj = {}
+
   // 當 deps array 觀注 value 參數，每次 value 改變就會執行 useEffect callback
   useEffect(() => {
     console.log('useEffect callback');
-  }, [value])
+  }, [obj])
 
   return <>
     <h1>App</h1>
