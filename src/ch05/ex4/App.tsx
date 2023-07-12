84c826fdd9bcd60a5e4b29b95f51d4d476da483e
Hank <cgh20xx@gmail.com>
2023-07-12 16:22:51 +0800

    feat: 新增測試 onClick 傳入 obj ，memo 過的 B 組件會在 obj 本身參考改變時重新 render，下個範例會優化。

--- src/ch05/ex4/App.tsx
+++ src/ch05/ex4/App.tsx
@@ -6,15 +6,18 @@ import { memo, useState } from 'react'
 // 以便在某些情況下通過記住結果來提高性能。這意味著 React 將跳過渲染組件，並重用上次渲染的結果。
 // memo 文件：https://react.dev/reference/react/memo
 
-// 新增測試 B 組件的 props 變化，看看被 memo 過的 B 組件會不會被更新
+// 新增測試 B 組件的 props 傳入 num，看看 memo 過的 B 組件會不會被更新：會
+// 新增測試 B 組件的 props 傳入 obj，看看 memo 過的 B 組件會不會被更新：會
 type PropB = {
   num: number
+  obj: { name: string }
 }
 
-const B: React.FC<PropB> = memo(({ num }) => {
+const B: React.FC<PropB> = memo(({ num, obj }) => {
   console.log('B render', num);
   return <>
     <h1>B num:{num}</h1>
+    <p>name: {obj.name}</p>
   </>
 })
 
@@ -22,12 +25,16 @@ const App: React.FC = () => {
   console.log('App render');
   const [value, setValue] = useState(false)
   const [num, setNum] = useState(0)
+  const [obj, setObj] = useState({ name: '' })
   return <>
     <h1>App</h1>
-    <B num={num}/>
+    <B num={num} obj={obj}/>
     <button onClick={() => {setValue(!value)}}>重新 render</button>
     {/* 當 num 被改變時，B 組件也會被更新。 */}
     <button onClick={() => {setNum(num + 1)}}>num  +1</button>
+    {/* 當 obj 被改變時，B 組件也會被更新，因為每次都是新的 obj，期望只有 obj.name 真的有更新才重新 render */}
+    {/* 注意：當第二次按下 name = hank 時還是會被更新，下個範例會優化。 */}
+    <button onClick={() => {setObj({name: 'hank'})}}>name = hank</button>
   </>
 }
 
