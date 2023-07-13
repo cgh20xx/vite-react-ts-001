feff5d6a245936630bad447fb68fb20b7d122b2b
Hank <cgh20xx@gmail.com>
2023-07-13 18:00:12 +0800

    feat: 新增 page Home、About 兩個 Route

--- src/ch06/ex1/App.tsx
+++ src/ch06/ex1/App.tsx
@@ -1,11 +1,26 @@
 import { Routes, Route } from 'react-router-dom'
 
+
+function About() {
+  return <>
+    <h1>Page About</h1>
+  </>
+}
+
+function Home() {
+  return <>
+    <h1>Page Home</h1>
+  </>
+}
+
+
 const App: React.FC = () => {
   return <>
-    <h1>React Router App</h1>
+    <h1>Header</h1>
     {/* <Route> 能有多個，只能包在 <Routes> 底下使用 */}
     <Routes>
-      <Route path="/" element={<App />}/>
+      <Route path="/" element={<Home />}/>
+      <Route path="/about" element={<About />}/>
     </Routes>
   </>
 }
